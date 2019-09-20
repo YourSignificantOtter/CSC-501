c3ee6989d8a9848162348acf9cada74515913ce9
Nick Herrmann <nicholas.herrmann94@gmail.com>
2019-09-20 14:44:14 -0400

    EXPDIST actually done, issues with the last update solved

--- csc501-lab1/sys/resched.c
+++ csc501-lab1/sys/resched.c
@@ -29,56 +29,102 @@ int resched()
 		int r = (int)tmp; //Cast to int because pprio is int
 		if(r < 0)
 			r = r * -1; //get the abs value of the expdev return
-		int min = MAXINT; //max prio val is 99
-		int qiter = q[rdyhead].qnext;
-		int toRun = MAXINT;
 
-		while(qiter != rdytail)
+		int qiter = q[rdyhead].qnext;
+		while(qiter == 0) {
+			qiter = q[qiter].qnext;
+		}
+		int min = MAXINT; //max prio val is 99
+		int toRun = MAXINT;
+		Bool greater = FALSE; //r is greater than the highest prio
+		Bool less = FALSE; //r is less than the lowest prio
+		Bool empty = FALSE; //rdy queue is empty
+
+		if(isempty(rdyhead))
 		{
+			toRun = 0; //run null proc
+			empty = TRUE;
+		}
+		else if(r >= q[q[rdytail].qprev].qkey) //Is random value greater than the highest priority?
+		{
+			toRun = q[rdytail].qprev;
+			greater = TRUE;
+		}
+		else if(r < q[qiter].qkey)  //Is random value less than the lowest priority?
+		{
+			do { //Round robin
+				toRun = qiter;
+				qiter = q[qiter].qnext;
+			}while(q[qiter].qkey == q[toRun].qkey);
+			less = TRUE;
+		}
+
+		qiter = q[rdyhead].qnext; //reset qiter
+		while(qiter != rdytail && !greater && !less && !empty) // Find the right proc to run if none of the above
+		{
+			if(qiter == 0) // null process
+			{
+				qiter = q[qiter].qnext;
+				continue;
+			}
+
 			if(q[qiter].qkey >= r)
 			{
-				if(q[qiter].qkey < min)
+				if(q[qiter].qkey <= min) //Round robin with the comparison operator
 				{
 					min = q[qiter].qkey;
 					toRun = qiter;
 				}
+				else
+					break; //The queue is ordered so prioorities will only get higher
 			}
 			qiter = q[qiter].qnext;
 		}
 
+		struct pentry *optr = &proctab[currpid];
+		struct pentry *nptr = &proctab[toRun];
+
 		#ifdef DEBUG_PRINT
-			kprintf("min: %d\ttoRun: %d\n", min, toRun);
+			kprintf("r: %d\n", r);
+//			kprintf("min: %d\ttoRun: %d\n", min, toRun);
+//			kprintf("currpid: %d\tnew pid: %d\n", currpid, toRun);
+			kprintf("Current Process: %s\tCurrent Prio: %d\n", optr->pname, optr->pprio);
+			kprintf("New Process: %s\tNew Prio: %d\n", nptr->pname, nptr->pprio);
 		#endif
 
-		struct pentry *optr = &proctab[currpid];
-		struct pentry *nptr;
-
-		if(toRun == MAXINT) //There exists no process with prio greater than r, switch to the process with the highest prio
+		//Compare the PID we have found with the currently running process
+		if(empty)
 		{
-			toRun = getfirst(rdyhead);//First item in the ordered queue will have highest prio
-			if(toRun == EMPTY) //No processes are wiating to run, run the nullprocess
-				toRun = 0;
+			if(optr->pstate == PRCURR)
+				return OK; //No context switch needed just continue this process 
+		}
+		else if(greater)
+		{
+			if(optr->pprio > q[toRun].qkey && optr->pprio >= r)
+				return OK; //The current process has higher priority and the randomly generated number is greater than all priorities
+		}
+		else if(less)
+		{
+			if(optr->pprio < q[toRun].qkey)
+			{
+				if(optr->pprio >= r)
+					return OK;
+			}
 		}
 
-		if(toRun == currpid) //No need for context switch
-			return OK;
-
+		if(optr->pprio >= r && optr->pprio < nptr->pprio)
+		{
+			return OK; //continue to run the current process
+		}
 		toRun = dequeue(toRun); //Pop the new process out the queue and insert the current process back in
+
 		if(optr->pstate == PRCURR)
 		{
 			optr->pstate = PRREADY;
-			insert(currpid, rdyhead, optr->pprio);
+			insert(currpid, rdyhead, optr->pprio); //We inserted the process into the queue up above
 		}
 
-		nptr = &proctab[toRun]; //New process to run		
 		nptr->pstate = PRCURR;
-
-		#ifdef DEBUG_PRINT
-			//Force a context switch to the new process
-			kprintf("currpid: %d\tnew pid: %d\n", currpid, toRun);
-			kprintf("Current Process: %s\n", optr->pname);
-			kprintf("New Process: %s\n", nptr->pname);
-		#endif
 
 		#ifdef	RTCLOCK
 			preempt = QUANTUM;		// reset preemption counter	
